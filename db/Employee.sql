42af9cc0e051cb5773b4f3fac8295e8831a0768a
Zi-on <zioniflores12@yahoo.com>
2021-07-24 15:55:05 -0500

    fixed db

--- db/Employee.sql
+++ db/Employee.sql
@@ -22,12 +22,12 @@ PRIMARY KEy (role_id)
 INSERT INTO role(title, salary, department_id)
 VALUES ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4), ("Accountant", 125000, 3);
 
-
 CREATE TABLE employee (
 id INT AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT NULL,
+manager_id INT NULL,
 PRIMARY KEY (id)
 );
 
